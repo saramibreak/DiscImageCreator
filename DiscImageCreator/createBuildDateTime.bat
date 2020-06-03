4188a475e81831cc36f7aeecfbfb89396e980868
saramibreak <upswbsbj_20150526@yahoo.co.jp>
2020-06-04 00:10:08 +0900

    replaced the space of hour to '0' if time is from 0:00:00 to 9:59:59

--- DiscImageCreator/createBuildDateTime.bat
+++ DiscImageCreator/createBuildDateTime.bat
@@ -5,4 +5,5 @@ set HEADERFILE=%~dp0buildDateTime.h
 echo #pragma once> "%HEADERFILE%"
 echo.>> "%HEADERFILE%"
 echo #define BUILD_DATE "%DATE:~-10,4%%DATE:~-5,2%%DATE:~-2%">> "%HEADERFILE%"
-echo #define BUILD_TIME "%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%">> "%HEADERFILE%"
+set TIME2=%TIME: =0%
+echo #define BUILD_TIME "%TIME2:~0,2%%TIME2:~3,2%%TIME2:~6,2%">> "%HEADERFILE%"
