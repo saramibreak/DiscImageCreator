d2f3a7f4ba56f90ec7aef9ea2f002004617edd18
saramibreak <upswbsbj_20150526@yahoo.co.jp>
2021-11-05 18:42:47 +0900

    - changed: "DATE" to "wmic os get LocalDateTime"

--- DiscImageCreator/createBuildDateTime.bat
+++ DiscImageCreator/createBuildDateTime.bat
@@ -4,6 +4,8 @@ set HEADERFILE=%~dp0buildDateTime.h
 
 echo #pragma once> "%HEADERFILE%"
 echo.>> "%HEADERFILE%"
-echo #define BUILD_DATE "%DATE:~-10,4%%DATE:~-5,2%%DATE:~-2%">> "%HEADERFILE%"
-set TIME2=%TIME: =0%
-echo #define BUILD_TIME "%TIME2:~0,2%%TIME2:~3,2%%TIME2:~6,2%">> "%HEADERFILE%"
+
+for /f %%a in ('wmic os get LocalDateTime ^| findstr \.') DO set CUR_DATE=%%a
+
+echo #define BUILD_DATE "%CUR_DATE:~0,8%">> "%HEADERFILE%"
+echo #define BUILD_TIME "%CUR_DATE:~8,6%">> "%HEADERFILE%"
