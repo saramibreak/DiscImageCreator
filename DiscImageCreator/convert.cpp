c6aa699e65cc4ffd84b6ba50fff1c15dfbad60ed
saramibreak <upswbsbj_20150526@yahoo.co.jp>
2024-01-01 00:07:17 +0900

    updated ChangeLog.txt, Copyright

--- DiscImageCreator/convert.cpp
+++ DiscImageCreator/convert.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2023 sarami
+ * Copyright 2011-2024 sarami
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
